d2ae01a4e08e19b899e9dcacc32d543bc361d6fe
morizyun <zxcvbnmkind@gmail.com>
2013-02-11 04:05:17 +0900

    Try to implement scp! methods, but did not work yet
    Implemented scp methods, and SSH remote chmod methods.
    but did not work yet.
    ERROR is:Net::SCP::Error - SCP did not finish successfully (1)
    Probably because MOV file is too large for net/scp module
    Maybe should not use this module
    TODO:filename must be yymmddhoge.mov,
    but now, the final file name is hoge.mov when input is hoge.mov

--- file_upload.rb
+++ file_upload.rb
@@ -10,30 +10,30 @@ ENV['TMPDIR'] = "./tmp/"
 @mes = ""
 
 get '/' do
-	#hamlテンプレートに飛ばす
-	#Fileのアップローダー
+	# hamlテンプレートに飛ばす
+	# Fileのアップローダー
 	haml :upload_form
 end
 
 post '/upload' do
 	#ページからアップロードされたファイルは
-	#params[:file][:tempfile]にFileオブジェクトとして格納される
+	# params[:file][:tempfile]にFileオブジェクトとして格納される
 	if params[:file]
-		#set some valiables for constructor
+		# set some valiables for constructor
 		file_path = params[:file][:tempfile].path
 		filename = params[:file][:filename]
 		upload_date = params[:date]
+		username = params[:togoserver_username]
 		passwd = params[:togoserver_pass]
 
 		begin
 			mov = MOVfile.new(file_path, filename, upload_date)
-			#mov.scp!(passwd)
-			mov.setPropaties
+			mov.upload!(username, passwd)
 			@nikki_text = mov.output
-		rescue ArgumentError => e
-			puts e.message
-			@mes = e.message
-			redirect "/"
+		#rescue ArgumentError => e
+			#puts e.message
+			#@mes = e.message
+			#redirect "/"
 		end
 		
 		@mes = "Upload completed!"
--- lib/movfile.rb
+++ lib/movfile.rb
@@ -3,62 +3,53 @@
 require "fileutils"
 require "open3"
 require "./lib/nikki_default"
+require "net/ssh"
+require "net/scp"
 
 class MOVfile
 	include Nikki
 
 	#アップロードサーバー
-	@@server = "togotv@togotv.dbcls.jp"
-	#サーバー上のmovie file格納場所path
+	#@@server = "togotv.dbcls.jp"
+	@@server = "192.168.11.9"
+	#サーバー上のtogotv公開フォルダ
 	# -*- !!!!!!Public folder!!!!!! -*-*
 	@@tv_path = "/var/www/togotv"
 
-	#set getter methods for debug
-	attr_reader :file_subst, :filename, :upload_date, :params
-	
-	def initialize(filepath, filename, date)
-		@tmpfile_path = filepath
+	def initialize(tmpfile_path, filename, date)
+		@tmpfile_path = tmpfile_path
 		@filename = filename
 		raise ArgumentError, "movファイルしか指定できません" unless @filename =~ /\.mov$/
 		rename_tmpfile
 		@upload_date = setDate(date.to_s)
 
-		#propertiesで情報を格納するHashをセット
+		# propertiesで情報を格納するHashをセット
 		@params = Hash.new
 	end
 
-	#TODO:各インスタンスメソッドの定義及びテスト
-	def setPropaties
-		pict_command = "ffmpeg -y -i #{filename} -f image2 -r 1 -t 0:0:0.001 -an #{@upload_date}_0.jpg"
-		stdin, stdout, stderr = Open3.popen3(pict_command)
-		info = stderr.read.encode("UTF-8", "Shift_JIS") #出力に日本語が含まれている...
-		stdin.close
-		stdout.close
-		stderr.close
-
-		properties(info)
-		setDuration
+	#TODO:implement
+	def upload!(username, pass)
+		scp!(username, pass)
+		puts @params
+		#setPropaties #TODO:remoteでこれを行うには？
+		chmod_remote!
 	end
 
-	#TODO:file_pathを指定すること
-	def scp!
-		system("scp #{@file_path} #{@@server}:")
-	end
-
+	# output nikki text
 	def output
 		@nikki = setNikki
 	end
 
-	#helper methods from movie_up.rb
 	private
 
+	# tempfileを元の名前にrenameする
 	def rename_tmpfile
 		new_filepath = File.dirname(@tmpfile_path) + "/" + @filename
 		File.rename(@tmpfile_path, new_filepath)
 		@file_path = new_filepath
 	end
 
-	#set @params from system call "ffmpeg"
+	# set @params from system call "ffmpeg"
 	def properties(info)
 	  info = info.split("\n")
 	  info.each do |line|
@@ -87,15 +78,43 @@ class MOVfile
 	  end
 		return nil
 	end
+
+	def setPropaties
+		pict_command = "ffmpeg -y -i #{@filename} -f image2 -r 1 -t 0:0:0.001 -an #{@upload_date}_0.jpg"
+		stdin, stdout, stderr = Open3.popen3(pict_command)
+		info = stderr.read.encode("UTF-8", "Shift_JIS") #出力に日本語が含まれている...
+		stdin.close
+		stdout.close
+		stderr.close
+
+		properties(info)
+		setDuration
+	end
 	
-	#setting date
+	#TODO:ここで動かない。動くようにする
+	def scp!(username, pass)
+		@server_path = "/#{@filename}"
+		Net::SCP.start(@@server, username, {:password => pass, :compression => true}) do |scp|
+			channel = scp.upload!(@file_path, "/")
+			channel.wait
+		end
+	end
+
+	#TODO:テスト未完
+	def chmod_remote!(username, pass)
+		Net::SSH.start(@@server, username, {:password => pass}) do |ssh|
+			puts ssh.exec! "chmod 664 #{@server_path}"
+		end
+	end
+
+	# setting date
 	def setDate(arg_date)
 		match_yyyymmdd = arg_date =~ /^\d{8}/ || @filename =~ /^\d{8}/
 		match_yymmdd = arg_date =~ /^\d{6}/ || @filename =~ /^\d{6}/
-		#match to "yyyymmdd" format -> return self
+		# match to "yyyymmdd" format -> return self
 		if match_yyyymmdd
 	    return $&
-		#match to "yymmdd" format -> return 20yymmdd
+		# match to "yymmdd" format -> return 20yymmdd
 		elsif match_yymmdd
 	    return "20" + $&
 		else
