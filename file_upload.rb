268a63c5f7e5189e1910caeeef255596cdfb2d58
Taizo_Ayase <zxcvbnmkind@gmail.com>
2013-07-22 14:59:11 +0900

    App is modified as class
    TogoUploaderApp class is created

--- file_upload.rb
+++ file_upload.rb
@@ -11,59 +11,61 @@ require './lib/file_controller'
 ENV['TMPDIR'] = './tmp/'
 @mes = ''
 
-get '/' do
-	# hamlテンプレートに飛ばす
-	# Fileのアップローダー
-	haml :upload_form
-end
+class TogoUploaderApp < Sinatra::Base
+	get '/' do
+		# hamlテンプレートに飛ばす
+		# Fileのアップローダー
+		haml :upload_form
+	end
 
-post '/upload' do
-	#ページからアップロードされたファイルは
-	# params[:file][:tempfile]にFileオブジェクトとして格納される
+	post '/upload' do
+		#ページからアップロードされたファイルは
+		# params[:file][:tempfile]にFileオブジェクトとして格納される
 
-  unless params[:file]
-		@mes = 'MOVファイルを指定してください'
-l		redirect '/'
-  end
-  
-  begin
-    controller = FileController.new(params)
-    controller.upload!
-    #@nikki_text
-  rescue Exception => e
-    ap e
-    puts e.message
-    @mes = e.message
-    redirect '/'
-  end
-    
+	  unless params[:file]
+			@mes = 'MOVファイルを指定してください'
+	l		redirect '/'
+	  end
+	  
+	  begin
+	    controller = FileController.new(params)
+	    controller.upload!
+	    #@nikki_text
+	  rescue Exception => e
+	    ap e
+	    puts e.message
+	    @mes = e.message
+	    redirect '/'
+	  end
+	    
 =begin
-### former version that do not use file controller
-	if params[:file]
-		# set some valiables for constructor
-		file_path = params[:file][:tempfile].path
-		filename = params[:file][:filename]
-		upload_date = params[:date]
-		username = params[:togoserver_username]
-		passwd = params[:togoserver_pass]
+	### former version that do not use file controller
+		if params[:file]
+			# set some valiables for constructor
+			file_path = params[:file][:tempfile].path
+			filename = params[:file][:filename]
+			upload_date = params[:date]
+			username = params[:togoserver_username]
+			passwd = params[:togoserver_pass]
 
-		begin
-			mov = MOVfile.new(file_path, filename, upload_date)
-			mov.upload!(username, passwd)
-			@nikki_text = mov.output
-		rescue ArgumentError => e
-			puts e.message
-			@mes = e.message
+			begin
+				mov = MOVfile.new(file_path, filename, upload_date)
+				mov.upload!(username, passwd)
+				@nikki_text = mov.output
+			rescue ArgumentError => e
+				puts e.message
+				@mes = e.message
+				redirect '/'
+			end
+			
+			@mes = 'Upload completed!'
+			haml :uploaded
+		else
+			@mes = 'MOVファイルを指定してください'
 			redirect '/'
 		end
-		
-		@mes = 'Upload completed!'
-		haml :uploaded
-	else
-		@mes = 'MOVファイルを指定してください'
-		redirect '/'
-	end
 =end
+	end
 end
 
 __END__
