818d6dd4c5781b81e3035983eb225be62731a037
Taizo_Ayase <zxcvbnmkind@gmail.com>
2013-08-28 17:38:53 +0900

    Add rsync shell script
    TODO:include this script into the upload system

---
+++ lib/rsync.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+# for movie and streaming
+rsync -a /home/togotv/togotv_uploader/moviefile_tmp /var/www/togotv/movie
+
+# for thumbnail
+rsync -a /home/togotv/togotv_uploader/thumbnail_tmp /var/www/togotv/images
