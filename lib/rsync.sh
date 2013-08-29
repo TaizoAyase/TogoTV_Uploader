62cf73ab176cdd24bc65bb2bde53bbb020c1c9ff
Taizo_Ayase <zxcvbnmkind@gmail.com>
2013-08-29 16:20:21 +0900

    Some debug in rsync script
    old version of rsync script is copying directory itself
    new version is copying files

--- lib/rsync.sh
+++ lib/rsync.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 # for movie and streaming
-rsync -a /home/togotv/togotv_uploader/moviefile_tmp /var/www/togotv/movie
+rsync -a /home/togotv/togotv_uploader/moviefile_tmp/ /var/www/togotv/movie
 
 # for thumbnail
-rsync -a /home/togotv/togotv_uploader/thumbnail_tmp /var/www/togotv/images
+rsync -a /home/togotv/togotv_uploader/thumbnail_tmp/ /var/www/togotv/images
